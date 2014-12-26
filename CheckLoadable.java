02130b777ffe0bdf4378aa816420c664a59f598f
Akhil Jain <akhiljain93>
2014-12-26 12:04:02 +0530

    add code to check memory

---
+++ CheckLoadable.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+
+public class CheckLoadable {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br1 = new BufferedReader(new FileReader("files"));
+		String f;
+		HashSet<Long> h = new HashSet<Long>();
+		while ((f = br1.readLine()) != null) {
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			String in;
+			while ((in = br.readLine()) != null) {
+				String s[] = in.split("\t");
+				if (!h.contains(Long.parseLong(s[0])))
+					h.add(Long.parseLong(s[0]));
+				if (!h.contains(Long.parseLong(s[2])))
+					h.add(Long.parseLong(s[2]));
+			}
+			br.close();
+			System.out.println("Done " + f);
+		}
+		br1.close();
+	}
+}
--- MakeTrees.java
+++ MakeTrees.java
@@ -12,37 +12,42 @@ class Node {
 
 	Node(long id) {
 		this.id = id;
+		this.children = new LinkedList<Node>();
 	}
 }
 
 public class MakeTrees {
 	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(
-				new FileReader(
-						"D:\\Study Material\\8th Sem\\CSD750\\akhil\\reply-data_0_1.txt"));
-		HashSet<String> h = new HashSet<String>();
+		BufferedReader br1 = new BufferedReader(new FileReader("files"));
+		String f;
+		HashSet<Long> h = new HashSet<Long>();
 		LinkedList<Node> done = new LinkedList<Node>();
-		String in;
-		while ((in = br.readLine()) != null) {
-			String s[] = in.split("\t");
-			Node parent = null;
-			if (h.contains(s[2])) {
-				ListIterator<Node> iter = done.listIterator();
-				while (iter.hasNext()) {
-					parent = iter.next();
-					if (parent.id == Long.parseLong(s[2]))
-						break;
+		while ((f = br1.readLine()) != null) {
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			String in;
+			while ((in = br.readLine()) != null) {
+				String s[] = in.split("\t");
+				Node parent = null;
+				if (h.contains(Long.parseLong(s[2]))) {
+					ListIterator<Node> iter = done.listIterator();
+					while (iter.hasNext()) {
+						parent = iter.next();
+						if (parent.id == Long.parseLong(s[2]))
+							break;
+					}
+				} else {
+					parent = new Node(Long.parseLong(s[2]));
+					h.add(Long.parseLong(s[2]));
+					done.add(parent);
 				}
-			} else {
-				parent = new Node(Long.parseLong(s[2]));
-				h.add(s[2]);
-				done.add(parent);
+				h.add(Long.parseLong(s[0]));
+				Node child = new Node(Long.parseLong(s[0]));
+				parent.children.add(child);
+				done.add(child);
 			}
-			h.add(s[0]);
-			Node child = new Node(Long.parseLong(s[0]));
-			parent.children.add(child);
-			done.add(child);
+			br.close();
+			System.out.println("Done " + f);
 		}
-		br.close();
+		br1.close();
 	}
 }
