8f4c1f74e22a470c943082d413172955c42bd83f
Akhil Jain <akhiljain93>
2015-03-02 09:15:46 +0530

    chunks modified according to new convention

--- fresh-start/ChunkedChildren.java
+++ fresh-start/ChunkedChildren.java
@@ -31,23 +31,11 @@ class Child implements Comparable<Child> {
 }
 
 public class ChunkedChildren {
-	static final long id[] = { 0l, 42500000000000000l, 85000000000000000l,
-			170000000000000000l, 232500000000000000l, 265000000000000000l,
-			295000000000000000l, 325000000000000000l, 355000000000000000l,
-			387500000000000000l, 400000000000000000l, 415000000000000000l,
-			430000000000000000l, 431000000000000000l, 432000000000000000l,
-			434000000000000000l, 436000000000000000l, 438000000000000000l,
-			440000000000000000l, 441250000000000000l, 442500000000000000l,
-			443750000000000000l, 445000000000000000l, 446250000000000000l,
-			447500000000000000l, 448750000000000000l, 450000000000000000l,
-			450750000000000000l, 451500000000000000l, 452250000000000000l,
-			453050000000000000l, 453750000000000000l, 454500000000000000l,
-			455250000000000000l, 456000000000000000l, 457300000000000000l,
-			458700000000000000l, 459300000000000000l, 460000000000000000l,
-			461000000000000000l, 5000000000000000000l };
+	static final long id[] = { 0l, 461000000000000000l, 470750000000000000l,
+			490250000000000000l, 5000000000000000000l };
 
 	public static void main(String args[]) throws IOException {
-		for (int i = 0; i < id.length; i++) {
+		for (int i = 1; i < id.length; i++) {
 			BufferedReader br = new BufferedReader(new FileReader("chunk_" + i));
 			Hashtable<Long, LinkedList<Child>> chunk = new Hashtable<Long, LinkedList<Child>>();
 			String in;
@@ -84,7 +72,8 @@ public class ChunkedChildren {
 			while (e.hasMoreElements()) {
 				long parent = e.nextElement();
 				LinkedList<Child> children = chunk.get(parent);
-				children = uniquify(children); //to ensure that the child list is unique
+				children = uniquify(children); // to ensure that the child list
+												// is unique
 				String toWrite = parent + "";
 				Iterator<Child> iter = children.iterator();
 				while (iter.hasNext()) {
